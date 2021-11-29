d4c00172b1f0b24617c02c892388fa18704f4aad
Andrew Collier <andrew@fathomdata.dev>
2021-11-29 10:16:29 +0000

    fix: Sort out "Possibly misspelled words in DESCRIPTION" on CRAN submission

---
+++ .aspell/defaults.R
@@ -0,0 +1,8 @@
+Rd_files <- vignettes <- R_files <- description <-
+  list(encoding = "UTF-8",
+       language = "en",
+       dictionaries = c(
+         "en_stats",
+         "filebin"
+       )
+  )
---
+++ R/update-aspell-wordlist.R
@@ -0,0 +1,11 @@
+# Updates the wordlist used by CRAN checks.
+
+library(here)
+
+WORDLIST <- here("inst", "WORDLIST")
+
+words <- readLines(WORDLIST)
+
+dir.create(".aspell", showWarnings = FALSE)
+
+saveRDS(WORDLIST, file = file.path(".aspell", "filebin.rds"), version = 2)
