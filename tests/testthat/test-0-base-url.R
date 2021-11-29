0d6de3d514a8ff09cc54f9a7d42b2d69f4f80567
Andrew Collier <andrew@fathomdata.dev>
2021-11-29 16:53:40 +0000

    chore: Add tests for base_url()

---
+++ tests/testthat/test-0-base-url.R
@@ -0,0 +1,11 @@
+test_that("set base URL", {
+  original_base_url <- base_url()
+  expect_equal(original_base_url, "https://filebin.net")
+
+  base_url <- "https://my-custom-filebin.net"
+
+  expect_equal(base_url(base_url), base_url)
+  expect_equal(base_url(), base_url)
+
+  base_url(original_base_url)
+})
